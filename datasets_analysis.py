bf07c4d8f087cad7b808242c08164300681a8a28
Sumit Kulhari <51313664+sumikbuckshot@users.noreply.github.com>
2019-06-21 16:43:43 +0530

    Add files via upload

---
+++ datasets_analysis.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from sklearn import datasets
+import time 
+
+
+# In[2]:
+
+
+#showing datasets
+for i in dir(datasets):
+    print(i)
+    time.sleep(2)
+
+
+# In[ ]:
+
+
+
+
